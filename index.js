963a9a958e533f72b9ee312089003c5f29fddca7
sergeymaguire <sergeymaguire@gmail.com>
2018-10-23 08:49:40 -0700

    A node application using constructors

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   res.end('Hello World\n');
-// });
+const server = http.createServer((req, res) => {
+  res.end('Hello World\n');
+});
 
-// server.listen(4242, () => {
-//   console.log('Server is running...');
-// });
+server.listen(4242, () => {
+  console.log('Server is running...');
+});
 
-// var guessesRemaining = 10  
-// var guessedLetters = []
-// var display = 0
-// var currentWord
+var guessesRemaining = 10  
+var guessedLetters = []
+var display = 0
+var currentWord
 
-// function startGame() {
-//   console.log('---------------------------------------------------------')
-//   console.log('')
-//   console.log('Welcome to Hangman!')
-//   console.log('')
-//   console.log('---------------------------------------------------------')
-//   if(guessedLetters.length > 0){
-//     guessedLetters = []
-//   }
+function startGame() {
+  console.log('---------------------------------------------------------')
+  console.log('')
+  console.log('Welcome to Hangman!')
+  console.log('')
+  console.log('---------------------------------------------------------')
+  if(guessedLetters.length > 0){
+    guessedLetters = []
+  }
 
-// const readline = require('readline');
+const readline = require('readline');
 
-// readline.emitKeypressEvents(process.stdin);
-// //process.stdin.setRawMode(true);
+readline.emitKeypressEvents(process.stdin);
 
-// process.stdin.on('keypress', (str, key) => {
-//   console.log("Your current guess: ");
-//   console.log(guessesRemaining);
-//   console.log(str);
-//   //console.log(key)
-// })
-// }
-// startGame();
+
+process.stdin.on('keypress', (str, key) => {
+
+ var letterGuessed = console.log("Your current guess: " + str);
+ if(letterGuessed === undefined){
+     console.log("Choose a letter please")
+ }
+ console.log(letterGuessed)
+  console.log(guessesRemaining);
+  if (guessesRemaining === 0){
+      console.log("You lose! Try Again!")
+      gameLost();
+  }
+  //console.log(str);
+  //if(str === undefined)
+  //console.log("Please pick a real letter")
+})
+}
+startGame();
 
 var readline = require('readline');
 
@@ -46,9 +56,9 @@ var rl = readline.createInterface({
 
 var recursiveAsyncReadLine = function () {
     rl.question("Please Choose an option:\n"
-        + "1) Option 1\n"
-        + "2) Option 2\n"
-        + "3) Exit\n"
+        + "1) Play the Game!\n"
+        + "2) Restart the Game\n"
+        + "3) Exit the game :(\n"
         , function (line) {
 
             switch (line){
@@ -64,7 +74,7 @@ var recursiveAsyncReadLine = function () {
                 default:
                     console.log("No such option. Please enter another: ");
             }
-    recursiveAsyncReadLine(); //Calling this function again to ask new question
+    recursiveAsyncReadLine();
     });
 };
 
